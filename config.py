bf90b857813986f5f5c3e7f1e4815f1c87cdae95
Trilink12 <154373433+Trilink12@users.noreply.github.com>
2024-05-26 11:15:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -93,9 +93,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://spotless-loree-trilink-93aadf44.koyeb.app/"
     else:
-        URL = ""
+        URL = "https://spotless-loree-trilink-93aadf44.koyeb.app/"
 
 
 
